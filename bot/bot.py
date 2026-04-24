005b1cbf747c78d9d9286dab15e8969a34188d72
Quan-tara <quantaraclaim@outlook.com>
2026-04-24 09:22:26 +0200

    Add files via upload

--- bot/bot.py
+++ bot/bot.py
@@ -16,7 +16,7 @@ import math
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 def is_admin(ctx): return ctx.author.id == ADMIN_ID
 
